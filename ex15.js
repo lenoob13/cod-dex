98a8d539eba744cee541694b16d463a80c6ee86d
Mathéo Gastel <117746376+lenoob13@users.noreply.github.com>
2025-03-13 11:19:43 +0100

    Create ex15.js
    ex 15 du cours de JavaScript sur codédex

---
+++ ex15.js
@@ -0,0 +1,22 @@
+// Write code below 💖
+// ex 15 du cours de Js sur codédex
+let rand=Math.random();
+let eight=Math.floor(rand*8);
+console.log("Question:     Should I continue ?");
+if (eight==0){
+  console.log("Magic eight ball: better not tell you ...");
+}else if(eight==1){
+  console.log("Magic eight ball: very nice !");
+}else if(eight==2){
+  console.log("Magic eight ball: you shouldn't.");
+}else if(eight==3){
+  console.log("Magic eight ball: why not ?");
+}else if(eight==4){
+  console.log("Magic eight ball: You have to do it !");
+}else if(eight==5){
+  console.log("Magic eight ball: go ahead");
+}else if(eight==6){
+  console.log("Magic eight ball: NO NO NO !");
+}else if(eight==7){
+  console.log("Magic eight ball: you're dommed ._.");
+}
